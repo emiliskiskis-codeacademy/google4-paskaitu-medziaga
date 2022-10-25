0151410991482053ad72c397a1c160307ef18838
Emilis Kiškis <emilis.kiskis@codeacademylt.onmicrosoft.com>
2022-10-25 19:19:30 +0300

    6 paskaita - JS intro

---
+++ Paskaitų kodas/js/01-intro/index.js
@@ -0,0 +1,57 @@
+/*
+  [x] paprastos operacijos
+  [ ] kintamieji
+*/
+
+// vienos eilutės komentaras
+
+// 2 + 3 = 5
+// 2 - 3 = -1
+// 2 * 3 = 6
+// 4 / 2 = 2
+// 3 ** 3 = 27
+
+// number
+console.log(2 + 3)
+console.log(2 - 3)
+console.log(2 * 3)
+console.log(4 / 2)
+
+console.log((1 + 2) * 4 * 5 + 6)
+
+// string
+console.log("Hello world!")
+
+console.log("5 + 3 rezultatas yra:")
+console.log(5 + 3)
+
+console.log("Hello" + "World")
+console.log("10 + 9 rezultatas yra: " + (10 + 9))
+
+document.write("10 + 9 rezultatas yra: " + (10 + 9))
+document.write(" 5 + 3 rezultatas yra: " + (5 + 3))
+
+/* Funkcijų sąrašas:
+  console.log
+  document.write
+  document.writeln
+  alert
+  confirm
+  prompt
+*/
+
+// alert("Jūs laimėjote naują iphone 4S!")
+// console.log("alert uždarytas")
+
+// console.log(confirm("Ar tikrai nenorite laimėti iphone 4S?"))
+// 1. atliks confirm ir lauks patvirtinimo
+// 2. išloggins funkcijos rezultatą
+
+// boolean
+console.log(true)
+console.log(false)
+
+console.log(prompt("Koks jūsų vardas?")) // tekstas arba null
+
+// null
+console.log(null)
